15d0e118edbc359e81a8dee8f20c9569750e6211
Agustin <agusdiaz654321@gmail.com>
2023-08-17 12:43:47 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,7 +1,6 @@
-// Obtener el div por su ID
+
 const div = document.getElementsByClassName("div");
 
-// Añadir el manejador de eventos
 div.addEventListener('click', function() {
     alert('Hola! Soy el div');
 });
